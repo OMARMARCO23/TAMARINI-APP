a46f73c9e1ffbd1564087cb4ab7b980820cb41fd
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-12-04 23:51:25 +0100

    Delete src directory

--- src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useTutorSession } from './useTutorSession';
-export { useLocalStorage } from './useLocalStorage';
-export { useProgress } from './useProgress';
-export { useOCR } from './useOCR';
